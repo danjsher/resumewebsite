44753ad0840395db4d3085ac4266ff72ea5d1c02
Dan <dsherida@student.umass.edu>
2016-01-02 01:09:52 -0500

    ..

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 var main = function() {
+
     $('.menu-icon').click(function() {
         
         $('.menu').animate({ top: '0px'}, 200);
         
         $('body').animate({top: '75px'}, 200);
+
+        $('.arrow-icon').rotate(270deg);
     });
+
+
+
 
 };
 
