43dad26859586828d8c7e23f44b9845c4cef6f95
root <root@ip-172-31-18-118.us-west-2.compute.internal>
2016-01-02 02:59:17 -0500

    initial structure and functionality

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
-img = document.getElementById('menu-icon');
-
 var main = function() {
 
-    $('.menu-icon').click(function() {
+    $('#arrow-down').click(function() {
         
         $('.menu').animate({ top: '0px'}, 200);
         
         $('body').animate({top: '75px'}, 200);
 
-        img.className = "rotate90";
+	$('#arrow-down').hide();
+	$('#arrow-up').show();
      });
 
-
-
-
+    $('#arrow-up').click(function() {
+        
+        $('.menu').animate({ top: '-75px'}, 200);
+        
+        $('body').animate({top: '0px'}, 200);
+	
+	$('#arrow-down').show();
+	$('#arrow-up').hide();
+     });
+    
 };
 
 $(document).ready(main);
